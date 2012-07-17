0f69ce31ee8a06e0fb00d95771ce63dd1c7358f9
robot <robot@robot-VirtualBox.(none)>
2012-07-17 14:52:10 +0200

    Commit inicial después de crear la aplicación

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cursos::Application.config.secret_token = '1e4e1b5745b18add748775ff81dda19dce4e5e24e7c44083c6ef5f217211da625a30beb8caf37870f41539245ceed12c4ff66035554c7b6685c8fcfc2fc18c8e'
