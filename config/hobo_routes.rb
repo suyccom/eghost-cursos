9f6801679b4a0c240f341ca4263ab6cc7bedc6b8
Ignacio Huerta <ignacio@ihuerta.net>
2012-07-17 14:57:23 +0200

    Generar los recursos para los alumnos y los cursos

--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -26,6 +26,26 @@ Cursos::Application.routes.draw do
   get 'logout(.:format)' => 'users#logout', :as => 'user_logout'
   match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
 
+
+  # Resource routes for controller "alumnos"
+  get 'alumnos(.:format)' => 'alumnos#index', :as => 'alumnos'
+  get 'alumnos/new(.:format)', :as => 'new_alumno'
+  get 'alumnos/:id/edit(.:format)' => 'alumnos#edit', :as => 'edit_alumno'
+  get 'alumnos/:id(.:format)' => 'alumnos#show', :as => 'alumno', :constraints => { :id => %r([^/.?]+) }
+  post 'alumnos(.:format)' => 'alumnos#create', :as => 'create_alumno'
+  put 'alumnos/:id(.:format)' => 'alumnos#update', :as => 'update_alumno', :constraints => { :id => %r([^/.?]+) }
+  delete 'alumnos/:id(.:format)' => 'alumnos#destroy', :as => 'destroy_alumno', :constraints => { :id => %r([^/.?]+) }
+
+
+  # Resource routes for controller "cursos"
+  get 'cursos(.:format)' => 'cursos#index', :as => 'cursos'
+  get 'cursos/new(.:format)', :as => 'new_curso'
+  get 'cursos/:id/edit(.:format)' => 'cursos#edit', :as => 'edit_curso'
+  get 'cursos/:id(.:format)' => 'cursos#show', :as => 'curso', :constraints => { :id => %r([^/.?]+) }
+  post 'cursos(.:format)' => 'cursos#create', :as => 'create_curso'
+  put 'cursos/:id(.:format)' => 'cursos#update', :as => 'update_curso', :constraints => { :id => %r([^/.?]+) }
+  delete 'cursos/:id(.:format)' => 'cursos#destroy', :as => 'destroy_curso', :constraints => { :id => %r([^/.?]+) }
+
   namespace :admin do
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120717124801) do
+ActiveRecord::Schema.define(:version => 20120717125610) do
+
+  create_table "alumnos", :force => true do |t|
+    t.string   "nombre"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "cursos", :force => true do |t|
+    t.string   "titulo"
+    t.date     "fecha"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "crypted_password",          :limit => 40
